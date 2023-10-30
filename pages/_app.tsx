8e4207c0ef70454f5687dc587fdc3918745411ba
Adam Hastings <adam@dteb.co.uk>
2023-10-30 17:41:12 +0000

    Updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "../constants/apollo-client";
 import Guard from "../components/Guard";
+import { Analytics } from '@vercel/analytics/react';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -11,7 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
       <ApolloProvider client={client}>
         <Guard excludedRoutes={["/login", "/signup"]}>
-          <Component {...pageProps} />
+          <><Component {...pageProps} /><Analytics /></>
         </Guard>
       </ApolloProvider>
   );
