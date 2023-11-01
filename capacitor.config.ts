6a6335adbb64c9ed0fb3e88f048a9aa26810f7a9
drinkteaeatbiscuits <40630245+drinkteaeatbiscuits@users.noreply.github.com>
2023-11-01 13:15:49 +0000

    Update webdir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'uk.co.topgift.app',
   appName: 'Top Gift',
-  webDir: 'out',
+  webDir: 'public',
   server: {
     androidScheme: 'https',
     url: "https://nextjs-auth-drinkteaeatbiscuits.vercel.app",
