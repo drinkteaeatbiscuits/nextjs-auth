d5b498c79f87ac40cd50865fb1dc58e8d4a569e7
drinkteaeatbiscuits <40630245+drinkteaeatbiscuits@users.noreply.github.com>
2023-11-02 10:48:47 +0000

    Updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,14 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: '3e0947af',
+  appId: 'D23H3WR3TP.uk.co.topgift.app',
   appName: 'Top Gift',
   webDir: 'public',
   server: {
     androidScheme: 'https',
     url: "https://nextjs-auth-drinkteaeatbiscuits.vercel.app",
     cleartext: true
-  }
+  },
 };
 
 export default config;
